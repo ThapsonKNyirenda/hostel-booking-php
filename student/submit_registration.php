d745c56f1e069fda8e160acd7ec8764885cd05dd
Thapson <132282346+ThapsonKNyirenda@users.noreply.github.com>
2024-07-27 21:21:56 +0200

    Adding student home page

--- student/submit_registration.php
+++ student/submit_registration.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_type'])) {
 $user_id = $_SESSION['user_id']; 
 
 // Database configuration
-include '../connection/dbconnection.php' ;
+include '../connection/dbconnection.php';
 
 // Collect form data
 $first_name = $_POST['first_name'];
@@ -28,16 +28,29 @@ $next_of_kin_address = $_POST['next_of_kin_address'];
 $next_of_kin_phone = $_POST['next_of_kin_phone'];
 $room_number = $_POST['room_number'];
 
+// Check if a row with the same owner already exists
+$check_stmt = $conn->prepare("SELECT * FROM applications WHERE owner = ?");
+$check_stmt->bind_param("s", $user_id);
+$check_stmt->execute();
+$check_result = $check_stmt->get_result();
+
+if ($check_result->num_rows > 0) {
+    // Row with the same owner already exists, show an error
+    header("Location: home.php?message=already_exist");
+    exit();
+}
+
 // Prepare SQL statement to prevent SQL injection
-$stmt = $conn->prepare("INSERT INTO applications (first_name, last_name, phone_number, gender, registration_date, home_address, course, next_of_kin_name, next_of_kin_current_address, next_of_kin_phone_number,owner, room_number) VALUES (?, ?, ?, ? , ? , ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO applications (first_name, last_name, phone_number, gender, registration_date, home_address, course, next_of_kin_name, next_of_kin_current_address, next_of_kin_phone_number, owner, room_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssssssssss", $first_name, $last_name, $phone_number, $gender, $registration_date, $home_address, $course, $next_of_kin_name, $next_of_kin_address, $next_of_kin_phone, $user_id, $room_number);
 
 // Execute the statement
 if ($stmt->execute()) {
     header("Location: home.php?message=success");
-        exit();
+    exit();
 } else {
-    echo "Error: " . $stmt->error;
+    header("Location: home.php?message=failed");
+    exit();
 }
 
 // Close the statement and connection
